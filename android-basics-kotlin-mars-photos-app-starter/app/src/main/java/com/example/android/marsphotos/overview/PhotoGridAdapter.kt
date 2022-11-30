fbce43807a6abc532f22a0faeab11cb2bb1e955d
unknown <103048480+Freya0119@users.noreply.github.com>
2022-11-30 17:58:47 +0800

    mars photos
    work in adapter
    
    https://developer.android.com/codelabs/basic-android-kotlin-training-internet-images?hl=zh-tw&continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-kotlin-unit-4-pathway-2%3Fhl%3Dzh-tw%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-training-internet-images#3

---
+++ android-basics-kotlin-mars-photos-app-starter/app/src/main/java/com/example/android/marsphotos/overview/PhotoGridAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.android.marsphotos.overview
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.android.marsphotos.databinding.FragmentOverviewBinding
+import com.example.android.marsphotos.databinding.GridViewItemBinding
+import com.example.android.marsphotos.network.MarsPhoto
+
+class PhotoGridAdapter :
+    ListAdapter<MarsPhoto, PhotoGridAdapter.MarsPhotoViewHolder>(DiffCallback) {
+    class MarsPhotoViewHolder(var binding: GridViewItemBinding) : ViewHolder(binding.root) {
+        fun bind(MarsPhoto: MarsPhoto) {
+            binding.photo = MarsPhoto
+            binding.executePendingBindings()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MarsPhotoViewHolder {
+        return MarsPhotoViewHolder(GridViewItemBinding.inflate(LayoutInflater.from(parent.context)))
+    }
+
+    override fun onBindViewHolder(holder: MarsPhotoViewHolder, position: Int) {
+        val photo = getItem(position)
+        holder.bind(photo)
+    }
+}
